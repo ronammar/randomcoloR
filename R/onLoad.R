ff3233191a03b6596be25fcfab29e1c6119f30da
ammarr <ron.ammar@bms.com>
2017-12-07 10:57:49 -0500

    Adding alternate color space to distinctColorPalette()

--- R/onLoad.R
+++ R/onLoad.R
@@ -2,8 +2,9 @@
 
 ct <- NULL
 ourColorSpace <- NULL
+alternateColorSpace <- NULL
 
-#' @import V8 colorspace stats methods
+#' @import V8 colorspace stats methods scales grDevices
 .onLoad <- function(libname, pkgname){
   ct <<- v8()
   ct$source(system.file("js/randomColor.js", package=pkgname))
@@ -11,4 +12,7 @@ ourColorSpace <- NULL
   # Compute a 30000 color spectrum and convert to LAB
   ourColorSpace <<- colorspace::RGB(runif(30000), runif(30000), runif(30000))
   ourColorSpace <<- as(ourColorSpace, "LAB")
+
+  # Alternate color palette
+  alternateColorSpace <<- t(unique(col2rgb(hue_pal(l=60:100)(1e3))))
 }
--- R/randomcolor.R
+++ R/randomcolor.R
@@ -16,8 +16,8 @@
 #' @export
 #' @import stringr
 randomColor <- function(count=1,
-                         hue=c(" ", "random", "red", "orange", "yellow", "green", "blue", "purple", "pink", "monochrome"),
-                         luminosity=c(" ", "random", "light", "bright", "dark")
+                        hue=c(" ", "random", "red", "orange", "yellow", "green", "blue", "purple", "pink", "monochrome"),
+                        luminosity=c(" ", "random", "light", "bright", "dark")
 ) {
   hue <- match.arg(hue)
   luminosity <- match.arg(luminosity)
@@ -34,10 +34,22 @@ randomColor <- function(count=1,
 #' For more info, also see https://en.wikipedia.org/wiki/Lab_color_space
 #'
 #' @param k number of colors (>= 1). May be ineffective for k > 40.
+#' @param altCol Use an alternate color space
 #' @return A character vector of k optimally distinct colors in hexadecimal codes.
 #' @export
-distinctColorPalette <-function(k=1) {
+distinctColorPalette <-function(k=1, altCol=FALSE) {
+  currentColorSpace <- ourColorSpace@coords
+  if (altCol) {
+    currentColorSpace <- alternateColorSpace
+  }
+
   # Set iter.max to 20 to avoid convergence warnings.
-  km <- kmeans(ourColorSpace@coords, k, iter.max=20)
-  unname(hex(LAB(km$centers)))
+  km <- kmeans(currentColorSpace, k, iter.max=20)
+  if (altCol) {
+    colors <- rgb(round(km$centers), maxColorValue=255)
+  } else {
+    colors <- unname(hex(LAB(km$centers)))
+  }
+
+  return(colors)
 }
